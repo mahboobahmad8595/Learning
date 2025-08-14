bc573c536cd39beeab6b3360f1155777bdf40647
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-08-14 15:15:59 +0500

    Primitive Done

--- Class 02/app.js
+++ Class 02/app.js
@@ -4,11 +4,13 @@
 // 1. is me rules define hote. un rules ke khilaf kya to error aye ga. 
 // 2. code ko convert karta he binary me.
 
-// interpreter ye code read karne ke sath sath excute karta rehta he.
+// 1. interpreter ye code read karne ke sath sath excute karta rehta he.
 // Note : is me jhan error aye ga to code read aur excute wahin ruk jaye ga. jb tk error ko solve na kya jaye code aage read aur excute nahi hoga
-// compiler ye all code read karta he uske baad excute karta he.
-// Note : is me interpreter ka baraks hota he.
-// jit : just in time compile
+
+// 2. compiler ye all code read karta he uske baad excute karta he.
+// Note : is me error aye ga to ye bhi code aage nahi barhaye ga lykin usko read kar ke apne pass save kar le ga. excute tb kare ga jb error solve hoga
+
+// 3. jit : just in time compile
 
 // binary 0 aur 1 ki language.
 // 0 : false
@@ -74,10 +76,47 @@
 // Snake  Case : first_name   (Back-End)
 // Pascal Case : FirstName    (React)
 
+//! Data
+
+// 1. Primitive Data      : Single value , Complex nahi hota
+// 2. Non-Primitive Data  : Multiples value , Complex hota hai
+
+//! Primitive Data
+
+// 1. String : "Hello World" 'Hello World' `Hello World`
+// 2. Number : 10;
+// 3. boolean : true / false
+// 4. undefined : var a; , var a = undefined
+// 5. null : null
+
+// Examples
+// var a = "Hello world"
+// var b = 20;
+// var c = true;
+// var d = undefined;
+// var e = null ;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+
+//! Non-Primitive Data
+
+// 1. Object     
+// 2. Array
+// 3. Function
+
+
+// 2. Array
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// console.log(fruits);
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// console.log(fruits[1]);
+
 //! Syntax
 
 // var a = 10;
 // console.log(a);
 // Note : input me variable name likhte hain. aur output me uski value show hoti he
-
-
---
+++ Class 03/app.js
@@ -0,0 +1,8 @@
+// var fruit1 = "Apple";
+// var fruit2 = "Mango";
+// var fruit3 = "Banana";
+
+// console.log(fruit1);
+// console.log(fruit2);
+// console.log(fruit3);
+
