a7c9e789023b29cd6b485f1bc1ee9606d5c4e7da
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-08-14 16:20:51 +0500

    Assignment Completed

---
+++ Assignment/app.js
@@ -0,0 +1,50 @@
+// Ans 1
+// alert("Hello World!");
+
+// Ans 2
+// alert("Error! Please enter a valid password");
+
+// Ans 3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Ans 4
+// alert("Welcome to JS Land...");
+// alert("Prevent this page from creating additional dialoges");
+
+// Ans 5
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Ans 6
+// var username;
+
+// Ans 7
+// var myName = "Mahboob Ahmad";
+
+// Ans 8
+// var message = "Hello World";
+// alert(message)
+
+// Ans 9
+// var name = "Mahboob Ahmad";
+// var age = "21 years old";
+// var degree = "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(degree);
+
+// Ans 10
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// Ans 11
+// var email = "mahboobahmad8595@gmail.com";
+// alert("My Email address is" + " " + email);
+
+// Ans 12
+// var book = "A Samrter way to learn JavaScript";
+// alert("I am trying to learn from a Book" +" "+book)
+
+// Ans 13
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Ans 14
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
