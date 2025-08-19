fb941bb918036e2c66b11ee61285cc5018bf16b4
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-08-19 19:25:56 +0500

    Done

---
+++ 02 Variable Keywords/app.js
@@ -0,0 +1,39 @@
+//! Decleration & initialization
+
+// 1. Declaration           Creating a variable and giving it a name.
+// 2. Initialization        Giving a variable its first value.
+// 3. Redeclaration         Creating the same variable again using its keyword.   Just allowed in var keyword
+// 4. Reinitialization      Assigning a new value to a variable.
+
+// Examples
+
+// 1. Declaration
+// var num;  
+// console.log(num);
+
+// 2. Initialization
+// var age = 20;  
+// console.log(age);
+
+// 3. Redeclaration
+// var city = "Lahore";  
+// var city = "Karachi";  
+// console.log(city);
+
+// 4. Reinitialization
+// var name = "Ali";  
+// name = "Ahmed";  
+// console.log(name);
+
+//! Syntax
+
+// var a = 10;
+// console.log(a);
+// Note : input me variable name likhte hain. aur output me uski value show hoti he
+
+//! Key Words For Create Variables
+
+// 1. None of these
+// 2. var
+// 3. let
+// 4. const
