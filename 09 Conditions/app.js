513a0d7b1940a54e028dd5ecd487f1f2654267c6
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-08-21 23:51:37 +0500

    Java Script Statements

--- 09 Conditions/app.js
+++ 09 Conditions/app.js
@@ -1,22 +1,34 @@
-//! If
+
+//! JavaScript Condition Statements
+
+// 1. IF                    1 condition              
+// 2. IF ELSE               2 options
+// 3. IF ELSE IF            Multiple Options
+
+// note : true → code runs , false → code doesn't run.
+
+//? 1. If
 
 // var userAge = 18;
 // var userInput = prompt("Enter Your Age")
+
 // if(userInput >= userAge){
 //     console.log("Adult");
 // }
 
-//! If & Else
+//? 2. If & Else
 
 // var userAge = 18;
 // var userInput = prompt("Enter Your Age")
+
 // if(userInput >= userAge){
 //     console.log("Adult");
-// }else{
+// }
+// else{
 //     console.log("Minor");
 // }
 
-//! If & Else If 
+//? 3. If & Else If 
 
 // var a1 = 90;
 // var a = 80;
@@ -26,12 +38,16 @@
 
 // if(userInput >= a1){
 //     console.log("Grade A1");
-// }else if(userInput >= a){
+// }
+// else if(userInput >= a){
 //     console.log("Grade A");
-// }else if(userInput >= b){
+// }
+// else if(userInput >= b){
 //     console.log("Grade b");
-// }else if(userInput >= c){
+// }
+// else if(userInput >= c){
 //     console.log("Grade C");
-// }else if(userInput < c){
+// }
+// else if(userInput < c){
 //     console.log("Fail");
 // }
