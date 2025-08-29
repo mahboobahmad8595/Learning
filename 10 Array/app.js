629c94c941fa78d4b355a0884c4b41f1ab966367
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-08-29 15:22:55 +0500

    Array Start

---
+++ 10 Array/app.js
@@ -0,0 +1,77 @@
+//! Data
+
+// 1. Primitive Data      : Single value , Complex nahi hota
+// 2. Non-Primitive Data  : Multiples value , Complex hota hai
+
+//! Non-Primitive Data
+
+// 1. Object     
+// 2. Array
+// 3. Function
+
+//!______________________{ Array} ___________________________\\
+
+
+//! Create Create Array
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// console.log(fruits);
+
+//! Terms
+
+// 1. Elements       Apple Mango Banana
+// 2. Indexing       0      1     2
+// 3. Seperator      ,
+
+//! Call Specific
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// console.log(fruits[0]);
+
+//! Check Length
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// console.log(fruits.length); // 3
+
+//! Change Value
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits[0] = "Orange"; 
+// console.log(fruits);
+
+//! Add
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits[3] = "Orange";
+// console.log(fruits);
+
+//! Methods
+
+// unshift()    → array ke start me value add karta hai
+// shift()      → array ke start se value remove karta hai
+// push()       → array ke end me new value add karta hai
+// pop()        → array ke end se last value remove karta hai
+
+//! Unshift
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits.unshift("Orange");
+// console.log(fruits);
+
+//! Shift
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits.shift();
+// console.log(fruits);
+
+//! Push
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits.push("Orange");
+// console.log(fruits);
+
+//! Pop
+
+// var fruits = ["Apple" , "Mango" , "Banana"];
+// fruits.pop();
+// console.log(fruits);
