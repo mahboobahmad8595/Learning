fb29bab0d52a3399fc72c9ec556589cdf9569dec
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-09-12 23:11:53 +0500

    Practice

--- 04 JavaScript/00 JS/Practice/app.js
+++ 04 JavaScript/00 JS/Practice/app.js
@@ -47,3 +47,68 @@
 //     console.log(name);      // Chale ga
 // }
 // console.log(name);          // Nahi chale ga
+
+
+//! Text
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var user = prompt("Convert");
+
+// if(user = "a"){
+//     console.log(1);
+// }
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = 4;
+// var e = 5;
+// var f = 6;
+// var g = 7;
+// var h = 8;
+// var i = 9;
+// var j = 10;
+// var k = 11;
+// var l = 12;
+// var m = 13;
+// var n = 14;
+// var o = 15;
+// var p = 16;
+// var q = 17;
+// var r = 18;
+// var s = 19;
+// var t = 20;
+// var u = 21;
+// var v = 22;
+// var w = 23;
+// var x = 24;
+// var y = 25;
+// var z = 26;
+
+// console.table(m , a , h , b , o , o , b);
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(var i = 10; i > 0 ; i--){
+//     console.log(i);
+// }
+
+// var table = prompt("Enter a Num");
+// for(var i = 1; i <= 10; i++){
+//     console.log(table + " x " + i + " = " + table * i);
+// }
+
+// var table = prompt("Enter a Num");
+// var times = prompt("How many time do you want to multiply")
+// for(var i = 1; i <= times; i++){
+//     console.log(table + " x " + i + " = " + table * i);
+// }
