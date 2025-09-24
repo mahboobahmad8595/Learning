2e5d9e67339ed3535a46ebb120c757ff71095f7f
mahboobahmad8595 <mahboobahmad8595@gmail.com>
2025-09-24 15:34:21 +0500

    Assignment 04

---
+++ Web-Development/04 JavaScript/00 JS/Assignment/Assignment 04/app.js
@@ -0,0 +1,54 @@
+//! Ans 01
+// var arr = [[] , [] , []];
+
+//! Ans 02
+// var arr = [
+//     [0 , 1  , 2 , 3],
+//     [1 , 0  , 1 , 2],
+//     [2 , 1  , 0 , 1]
+// ]
+
+// console.log(arr);
+
+//! Ans 03
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+//! Ans 04
+// var table = prompt("Enter a Num");
+// var times = prompt("How many time do you want to multiply")
+// for(var i = 1; i <= times; i++){
+//     console.log(table + " x " + i + " = " + table * i);
+// }
+
+//! Ans 05
+
+// var fruits = ["Apple" , "Mango" , "Banana" , "Orange" , "Strawberry"];
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+//! Ans 06
+
+// for(var i = 1; i <= 15; i++){
+//     console.log(i);
+// }
+
+// for(var i = 10; i > 0 ; i--){
+//     console.log(i);
+// }
+
+// for(var i = 0; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+// for(var i = 1; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+// for(var i = 2; i <= 20; i += 2){
+//     console.log(i + "K");
+// }
+
+//! Ans 07
